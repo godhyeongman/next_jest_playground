4a516b4a0acf145494443c671b02fd14dfeb0ec0
godhyeongman <aaaa4000@naver.com>
2023-04-19 21:23:45 +0900

    :wrench: 자동 jest-dom import 설정 추가

--- jest.config.js
+++ jest.config.js
@@ -10,8 +10,7 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const customJestConfig = {
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
+  setupFilesAfterEnv: ['<rootDir>/config/setupTests.js'],
   testEnvironment: 'jest-environment-jsdom',
 };
 
