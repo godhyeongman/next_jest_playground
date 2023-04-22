d291d626625fb9dbc7d04c496d349fc97a3f82fa
godhyeongman <aaaa4000@naver.com>
2023-04-23 00:00:56 +0900

    :sparkles: popover 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
+import { type } from 'os';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 function SundaeIceCream() {
   return (
@@ -21,9 +23,18 @@ export function OrderSummary() {
 
 export function SummaryForm() {
   const [checked, setChecked] = useState(false);
+  const [hover, setHover] = useState(false);
 
   const onChangeCheckbox = () => {
     setChecked(!checked);
+  };
+
+  const onEnterCheckbox = () => {
+    setHover(true);
+  };
+
+  const onLeaveCheckbox = () => {
+    setHover(false);
   };
 
   return (
@@ -33,14 +44,33 @@ export function SummaryForm() {
           type="checkbox"
           id="terms-and-conditions"
           onChange={onChangeCheckbox}
+          onMouseEnter={onEnterCheckbox}
+          onMouseLeave={onLeaveCheckbox}
         />
         I agree to Terms and Conditions
       </label>
       <button type="submit" disabled={!checked}>
         Confirm Order
       </button>
+      <Popover isHover={hover} />
     </form>
   );
 }
+
+type PopoverProps = { isHover: boolean };
+
+const Popover = ({ isHover }: PopoverProps) => {
+  return (
+    <PopoverWrapper isHover={isHover}>
+      <PopoverBody>No ice cream will deliverd</PopoverBody>
+    </PopoverWrapper>
+  );
+};
+
+const PopoverWrapper = styled.div<{ isHover: boolean }>`
+  display: ${isHover => (isHover ? 'block' : 'none')};
+`;
+
+const PopoverBody = styled.span``;
 
 export default SundaeIceCream;
