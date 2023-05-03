5f617759d32be62ce972dc5e4a08dd115d9495af
godhyeongman <aaaa4000@naver.com>
2023-05-03 22:40:06 +0900

    :sparkles: Option 컴포넌트 구현

--- src/components/Options/Options.tsx
+++ src/components/Options/Options.tsx
@@ -1,17 +1,29 @@
 import { useEffect, useState } from 'react';
+import ScoopOption from './ScoopOptions';
+import type { ScoopProps } from './ScoopOptions';
 
 type OptionsProps = {
   optionType: 'scoops' | 'toppings';
 };
 
 export default function Options({ optionType }: OptionsProps) {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState<ScoopProps[] | []>([]);
 
   useEffect(() => {
     fetch(`http://localhost:3000/${optionType}`)
       .then(res => res.json())
-      .then(setItems);
-  });
+      .then(setItems)
+      .catch(err => {
+        console.error(err);
+      });
+  }, [optionType]);
 
+  //TODO: null 토핑 옵션컴포넌트로 교체
+  const ItemComponent: React.FC<ScoopProps> | null =
+    optionType === 'scoops' ? ScoopOption : null;
+
+  const optionItems: JSX.Element[] | null = items.map(({ name, imagePath }) => (
+    <ItemComponent name={name} imagePath={imagePath} key={name} />
+  ));
   return <div />;
 }
