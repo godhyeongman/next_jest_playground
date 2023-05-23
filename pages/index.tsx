911ae87b02ed7da72e41f5e70bb34baba863b04f
godhyeongman <aaaa4000@naver.com>
2023-05-23 22:51:33 +0900

    :coffin: Remove unused dead code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,6 @@
 import { OrderSummary, SummaryForm } from '../src/components/OrderForm';
 import Options from '../src/components/Options/Options';
 import { useEffect } from 'react';
-import axios from 'axios';
-
-const getData = async () => {
-  await axios
-    .get(`http://localhost:3000/scoops`)
-    .then(res => console.log(res.data))
-    .catch(err => {
-      console.error(err);
-    });
-};
 
 function SundaeIceCream() {
   useEffect(() => {
